5af710125bb09c728f66b8149a5fddd787071a0c
agub <shintrfc@gmail.com>
2021-08-09 22:46:00 +0300

    react bootstrap setup & header and footer edit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,18 @@
+import Header from './components/Header'
+import Footer from './components/Footer'
+import { Container } from 'react-bootstrap'
+
 function App() {
 	return (
 		<>
-			<h1>Welcome to Umashio shop</h1>
+			<Header />
+			<main className='py-3'>
+				<Container>
+					<h1>旨い塩ショップ</h1>
+				</Container>
+			</main>
+
+			<Footer />
 		</>
 	)
 }
