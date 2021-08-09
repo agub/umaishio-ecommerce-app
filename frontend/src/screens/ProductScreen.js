bd21af5af2b44f1f743526549abdd481c177c36a
agub <shintrfc@gmail.com>
2021-08-10 00:58:46 +0300

    product detail screen implemented

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,7 +1,71 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
+import Rating from '../components/Rating'
+import products from '../products'
 
-const ProductScreen = () => {
-	return <div>product screen</div>
+const ProductScreen = ({ match }) => {
+	const product = products.find((p) => p._id === match.params.id)
+	return (
+		<>
+			<Link className='btn btn-light my-3' to='/'>
+				戻る
+			</Link>
+			<Row>
+				<Col md={5}>
+					<Image src={product.image} alt={product.name} fluid />
+				</Col>
+				<Col md={4}>
+					<ListGroup variant='flush'>
+						<ListGroup.Item as='h3'>{product.name}</ListGroup.Item>
+						<ListGroup.Item>
+							<Rating
+								value={product.rating}
+								text={`${product.numReviews} reviews`}
+							/>
+						</ListGroup.Item>
+						<ListGroup.Item>¥{product.price}</ListGroup.Item>
+						<ListGroup.Item>{product.description}</ListGroup.Item>
+					</ListGroup>
+				</Col>
+				<Col md={3}>
+					<Card>
+						<ListGroup variant='flush'>
+							<ListGroup.Item>
+								<Row>
+									<Col>値段: </Col>
+									<Col>
+										<strong>¥{product.price}</strong>
+									</Col>
+								</Row>
+							</ListGroup.Item>
+							<ListGroup.Item>
+								<Row>
+									<Col>Status: </Col>
+									<Col>
+										{product.countInStock > 0
+											? '在庫あり'
+											: '在庫なし'}
+									</Col>
+								</Row>
+							</ListGroup.Item>
+							<ListGroup.Item>
+								<Button
+									// className='my-auto cl-12'
+									className='btn my-20 w-100 custombtn'
+									type='button'
+									disabled={product.countInStock === 0}
+								>
+									カートへ入れる
+								</Button>
+								　
+							</ListGroup.Item>
+						</ListGroup>
+					</Card>
+				</Col>
+			</Row>
+		</>
+	)
 }
 
 export default ProductScreen
