27b6673e3ceba0dc4ee9984f00b0a0dc36dd9f11
agub <shintrfc@gmail.com>
2021-08-17 05:44:37 +0300

    adding redux

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,41 @@
+import express from 'express'
+import asyncHandler from 'express-async-handler'
+const router = express.Router()
+import Product from '../models/productModel.js'
+
+// @description   fetch all products
+// @route         GET /api/products
+// @access        Public
+router.get(
+	'/',
+	asyncHandler(async (req, res) => {
+		const products = await Product.find({})
+		res.json(products)
+	})
+)
+
+// @description   fetch single products
+// @route         GET /api/products/:id
+// @access        Public
+router.get(
+	'/:id',
+	asyncHandler(async (req, res) => {
+		// if (req.params.id.match(/^[0-9a-fA-F]{24}$/)) {
+		// 	const product = await Product.findById(req.params.id)
+		// 	if (product) {
+		// 		res.json(product)
+		// 	}
+		// } else {
+		// 	res.status(404).json({ message: 'Product not found' })
+		// }
+		const product = await Product.findById(req.params.id)
+		if (product) {
+			res.json(product)
+		} else {
+			res.status(404)
+			throw new Error('Product not found')
+		}
+	})
+)
+
+export default router
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,80 +1,151 @@
 import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
-import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import {
+	Row,
+	Col,
+	Image,
+	ListGroup,
+	Card,
+	Button,
+	ListGroupItem,
+	Form,
+} from 'react-bootstrap'
 import Rating from '../components/Rating'
-import axios from 'axios'
-// import products from '../products'
+import Message from '../components/Message'
+import Loader from '../components/Loader'
+import { listProductDetails } from '../actions/productActions'
 
-const ProductScreen = ({ match }) => {
-	// const product = products.find((p) => p._id === match.params.id)
-	const [product, setProduct] = useState({})
+const ProductScreen = ({ match, history }) => {
+	const [qty, setQty] = useState(0)
+
+	const dispatch = useDispatch()
+
+	const productDetails = useSelector((state) => state.productDetails)
+	const { loading, error, product } = productDetails
+
 	useEffect(() => {
-		const fetchProduct = async () => {
-			const { data } = await axios.get(`/api/products/${match.params.id}`)
-			setProduct(data)
-		}
-		fetchProduct()
-	}, [match])
+		dispatch(listProductDetails(match.params.id))
+	}, [dispatch, match])
+
+	const addToCartHandler = () => {
+		history.push(`/cart/${match.params.id}?qty=${qty}`)
+	}
+
 	return (
 		<>
 			<Link className='btn btn-light my-3' to='/'>
 				戻る
 			</Link>
-			<Row>
-				<Col md={5}>
-					<Image src={product.image} alt={product.name} fluid />
-				</Col>
-				<Col md={4}>
-					<ListGroup variant='flush'>
-						<ListGroup.Item as='h3'>{product.name}</ListGroup.Item>
-						<ListGroup.Item>
-							{product.rating && (
-								<Rating
-									value={product.rating}
-									text={`${product.numReviews} reviews`}
-								/>
-							)}
-						</ListGroup.Item>
-						<ListGroup.Item>¥{product.price}</ListGroup.Item>
-						<ListGroup.Item>{product.description}</ListGroup.Item>
-					</ListGroup>
-				</Col>
-				<Col md={3}>
-					<Card>
+			{loading ? (
+				<Loader />
+			) : error ? (
+				<Message variant='danger'>{error}</Message>
+			) : (
+				<Row>
+					<Col md={5} className='product-page-section'>
+						<Image src={product.image} alt={product.name} fluid />
+					</Col>
+					<Col md={4} className='product-page-section'>
 						<ListGroup variant='flush'>
-							<ListGroup.Item>
-								<Row>
-									<Col>値段: </Col>
-									<Col>
-										<strong>¥{product.price}</strong>
-									</Col>
-								</Row>
+							<ListGroup.Item as='h3'>
+								{product.name}
 							</ListGroup.Item>
 							<ListGroup.Item>
-								<Row>
-									<Col>Status: </Col>
-									<Col>
-										{product.countInStock > 0
-											? '在庫あり'
-											: '在庫なし'}
-									</Col>
-								</Row>
+								{product.rating && (
+									<Rating
+										value={product.rating}
+										text={`${product.numReviews} reviews`}
+									/>
+								)}
 							</ListGroup.Item>
+							<ListGroup.Item>¥{product.price}</ListGroup.Item>
 							<ListGroup.Item>
-								<Button
-									// className='my-auto cl-12'
-									className='btn my-20 w-100 custombtn'
-									type='button'
-									disabled={product.countInStock === 0}
-								>
-									カートへ入れる
-								</Button>
-								　
+								{product.description}
 							</ListGroup.Item>
 						</ListGroup>
-					</Card>
-				</Col>
-			</Row>
+					</Col>
+
+					<Col md={3} className='product-page-section'>
+						{/* from here > */}
+						<Card>
+							<ListGroup variant='flush'>
+								<ListGroup.Item>
+									<Row>
+										<Col className='product-page-section'>
+											値段:{' '}
+										</Col>
+										<Col className='product-page-section'>
+											<strong>¥{product.price}</strong>
+										</Col>
+									</Row>
+								</ListGroup.Item>
+								<ListGroup.Item>
+									<Row>
+										<Col className='product-page-section'>
+											Status:{' '}
+										</Col>
+										<Col className='product-page-section'>
+											{product.countInStock > 0
+												? '在庫あり'
+												: '在庫なし'}
+										</Col>
+									</Row>
+								</ListGroup.Item>
+
+								{product.countInStock > 0 && (
+									<ListGroup.Item>
+										<Row>
+											<Col className='product-page-section'>
+												個数:
+											</Col>
+											<Col className='product-page-section'>
+												<Form.Control
+													className='form-control-select'
+													style={{
+														padding:
+															'0.75rem 0.2rem 0.75rem 0.4rem',
+													}}
+													as='select'
+													value={qty}
+													onChange={(e) =>
+														setQty(e.target.value)
+													}
+												>
+													{[
+														...Array(
+															product.countInStock
+														).keys(),
+													].map((x) => (
+														<option
+															key={x + 1}
+															value={x + 1}
+														>
+															{x + 1}
+														</option>
+													))}
+												</Form.Control>
+											</Col>
+										</Row>
+									</ListGroup.Item>
+								)}
+
+								<ListGroup.Item>
+									<Button
+										onClick={addToCartHandler}
+										className='btn my-20 w-100 custombtn'
+										type='button'
+										disabled={product.countInStock === 0}
+									>
+										カートへ入れる
+									</Button>
+									　
+								</ListGroup.Item>
+							</ListGroup>
+						</Card>
+					</Col>
+				</Row>
+			)}
 		</>
 	)
 }
