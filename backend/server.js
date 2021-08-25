3fa25f68a13d4b625bd737e31b849ba296f8f5ed
agub <shintrfc@gmail.com>
2021-08-25 03:49:55 +0300

    builded

--- backend/server.js
+++ backend/server.js
@@ -29,10 +29,6 @@ if (process.env.NODE_ENV === 'development') {
 app.use(express.json())
 app.use(cors())
 
-app.get('/', (req, res) => {
-	res.send('Api is running on port 5000!!!!')
-})
-
 //googleauth
 // app.use(
 // 	session({
@@ -56,6 +52,18 @@ app.use('/api/upload', uploadRoutes)
 
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+	app.get('*', (req, res) =>
+		res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+	)
+} else {
+	app.get('/', (req, res) => {
+		res.send('API is running....')
+	})
+}
 
 app.use(notFound)
 app.use(errorHandler)
