e920860606e895baec649495cfe18de00a4ee273
agub <shintrfc@gmail.com>
2021-09-16 14:30:34 +0300

    productScreen has refactoring

---
+++ frontend/src/components/CustomerReview.js
@@ -0,0 +1,135 @@
+import React, { useState, useEffect } from 'react'
+import { Row, Col, Button, Form } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import Rating from './Rating'
+import Message from './Message'
+import Loader from './Loader'
+import { PRODUCT_CREATE_REVIEW_RESET } from '../constants/productConstants'
+import { createProductReview } from '../actions/productActions'
+
+const CustomerReview = ({ product, userInfo, match }) => {
+	const dispatch = useDispatch()
+	const [rating, setRating] = useState(0)
+	const [comment, setComment] = useState('')
+
+	const productReviewCreate = useSelector(
+		(state) => state.productReviewCreate
+	)
+	const {
+		success: successProductReview,
+		error: errorProductReview,
+		loading: loadingProductReview,
+	} = productReviewCreate
+
+	useEffect(() => {
+		if (successProductReview) {
+			// alert('review submitted')
+			setRating(0)
+			setComment('')
+			dispatch({ type: PRODUCT_CREATE_REVIEW_RESET })
+		}
+	}, [dispatch, match, successProductReview])
+
+	const submitHandler = (e) => {
+		e.preventDefault()
+		dispatch(createProductReview(match.params.id, { rating, comment }))
+	}
+	return (
+		<>
+			<div className='product-page-review-wrap'>
+				<Row className='product-page-review-container'>
+					<Col>
+						<h3 className='mt-3'>カスタマーレビュー</h3>
+						<div variant='flush'>
+							{product.reviews.length === 0 && (
+								<Message>評価なし</Message>
+							)}
+							{product.reviews.map((review) => (
+								<div key={review._id}>
+									<strong>{review.name}</strong>
+									<Rating value={review.rating} />
+									<p
+										style={{
+											'fontSize': '0.7rem',
+										}}
+									>
+										{review.createdAt.substring(0, 10)}
+									</p>
+									<p>{review.comment}</p>
+								</div>
+							))}
+
+							{loadingProductReview && <Loader />}
+							{errorProductReview && (
+								<Message variant='danger'>
+									{errorProductReview}
+								</Message>
+							)}
+							{userInfo && (
+								// !hasReviewed ? (
+								<>
+									{/* <h2 className='mt-3'>
+                レビュー
+              </h2> */}
+									{successProductReview && (
+										<Message variant='success'>
+											レビューを書き込みました。
+										</Message>
+									)}
+									<Form onSubmit={submitHandler}>
+										<Form.Group controlId='rating'>
+											<Form.Label>評価</Form.Label>
+											<Form.Control
+												className='product-page-review-form'
+												as='select'
+												value={rating}
+												onChange={(e) =>
+													setRating(e.target.value)
+												}
+											>
+												<option value=''>
+													選択してください
+												</option>
+												<option value='1'>★</option>
+												<option value='2'>★★</option>
+												<option value='3'>★★★</option>
+												<option value='4'>★★★★</option>
+												<option value='5'>★★★★★</option>
+											</Form.Control>
+										</Form.Group>
+										<Form.Group controlId='comment'>
+											<Form.Label>コメント</Form.Label>
+											<Form.Control
+												as='textarea'
+												row='3'
+												className='product-page-review-form'
+												value={comment}
+												onChange={(e) =>
+													setComment(e.target.value)
+												}
+											></Form.Control>
+										</Form.Group>
+
+										<Button
+											disabled={
+												loadingProductReview ||
+												comment === ''
+											}
+											className='product-page-button mt-3 w-100'
+											type='submit'
+											variant='primary'
+										>
+											投稿
+										</Button>
+									</Form>
+								</>
+							)}
+						</div>
+					</Col>
+				</Row>
+			</div>
+		</>
+	)
+}
+
+export default CustomerReview
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,22 +1,18 @@
 import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
-import { Row, Col, Image, ListGroup, Card, Button, Form } from 'react-bootstrap'
+import { Row, Col, Image, ListGroup, Button } from 'react-bootstrap'
 import Rating from '../components/Rating'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
 import Meta from '../components/Meta'
 import { addToCart } from '../actions/cartActions'
-import {
-	listProductDetails,
-	createProductReview,
-} from '../actions/productActions'
-import { PRODUCT_CREATE_REVIEW_RESET } from '../constants/productConstants'
+import { listProductDetails } from '../actions/productActions'
+
+import CustomerReview from '../components/CustomerReview'
 
 const ProductScreen = ({ match, history }) => {
 	const [qty, setQty] = useState(1)
-	const [rating, setRating] = useState(0)
-	const [comment, setComment] = useState('')
 
 	const [showComment, setShowComment] = useState(false)
 
@@ -30,30 +26,9 @@ const ProductScreen = ({ match, history }) => {
 	const userLogin = useSelector((state) => state.userLogin)
 	const { userInfo } = userLogin
 
-	const productReviewCreate = useSelector(
-		(state) => state.productReviewCreate
-	)
-	const {
-		success: successProductReview,
-		error: errorProductReview,
-		loading: loadingProductReview,
-	} = productReviewCreate
-
 	useEffect(() => {
-		if (successProductReview) {
-			// alert('review submitted')
-			setRating(0)
-			setComment('')
-			dispatch({ type: PRODUCT_CREATE_REVIEW_RESET })
-		}
 		dispatch(listProductDetails(match.params.id))
-	}, [dispatch, match, successProductReview])
-
-	// const hasReviewed = {
-	// 	if(userInfo) {
-	// 		product.reviews.find((r) => r.user === userInfo._id)
-	// 	},
-	// }
+	}, [dispatch, match])
 
 	const directToReview = () => {
 		document.getElementById('reviewComp').scrollIntoView()
@@ -64,12 +39,6 @@ const ProductScreen = ({ match, history }) => {
 		setMessage('買い物カゴへ追加しました。')
 		history.push(`/cart/${match.params.id}?qty=${qty}`)
 	}
-
-	const submitHandler = (e) => {
-		e.preventDefault()
-		dispatch(createProductReview(match.params.id, { rating, comment }))
-	}
-
 	return (
 		<>
 			{/* <Link className='btn btn-light my-3' to='/'>
@@ -145,169 +114,16 @@ const ProductScreen = ({ match, history }) => {
 								<div>
 									<div variant='flush' id='reviewComp'>
 										{showComment && (
-											<div className='product-page-review-wrap'>
-												<Row className='product-page-review-container'>
-													<Col>
-														<h3 className='mt-3'>
-															カスタマーレビュー
-														</h3>
-														<div variant='flush'>
-															{product.reviews
-																.length ===
-																0 && (
-																<Message>
-																	評価なし
-																</Message>
-															)}
-															{product.reviews.map(
-																(review) => (
-																	<div
-																		key={
-																			review._id
-																		}
-																	>
-																		<strong>
-																			{
-																				review.name
-																			}
-																		</strong>
-																		<Rating
-																			value={
-																				review.rating
-																			}
-																		/>
-																		<p
-																			style={{
-																				'fontSize':
-																					'0.7rem',
-																			}}
-																		>
-																			{review.createdAt.substring(
-																				0,
-																				10
-																			)}
-																		</p>
-																		<p>
-																			{
-																				review.comment
-																			}
-																		</p>
-																	</div>
-																)
-															)}
-
-															{loadingProductReview && (
-																<Loader />
-															)}
-															{errorProductReview && (
-																<Message variant='danger'>
-																	{
-																		errorProductReview
-																	}
-																</Message>
-															)}
-															{userInfo && (
-																// !hasReviewed ? (
-																<>
-																	{/* <h2 className='mt-3'>
-																			レビュー
-																		</h2> */}
-																	{successProductReview && (
-																		<Message variant='success'>
-																			レビューを書き込みました。
-																		</Message>
-																	)}
-																	<Form
-																		onSubmit={
-																			submitHandler
-																		}
-																	>
-																		<Form.Group controlId='rating'>
-																			<Form.Label>
-																				評価
-																			</Form.Label>
-																			<Form.Control
-																				className='product-page-review-form'
-																				as='select'
-																				value={
-																					rating
-																				}
-																				onChange={(
-																					e
-																				) =>
-																					setRating(
-																						e
-																							.target
-																							.value
-																					)
-																				}
-																			>
-																				<option value=''>
-																					選択してください
-																				</option>
-																				<option value='1'>
-																					★
-																				</option>
-																				<option value='2'>
-																					★★
-																				</option>
-																				<option value='3'>
-																					★★★
-																				</option>
-																				<option value='4'>
-																					★★★★
-																				</option>
-																				<option value='5'>
-																					★★★★★
-																				</option>
-																			</Form.Control>
-																		</Form.Group>
-																		<Form.Group controlId='comment'>
-																			<Form.Label>
-																				コメント
-																			</Form.Label>
-																			<Form.Control
-																				as='textarea'
-																				row='3'
-																				className='product-page-review-form'
-																				value={
-																					comment
-																				}
-																				onChange={(
-																					e
-																				) =>
-																					setComment(
-																						e
-																							.target
-																							.value
-																					)
-																				}
-																			></Form.Control>
-																		</Form.Group>
-
-																		<Button
-																			disabled={
-																				loadingProductReview ||
-																				comment ===
-																					''
-																			}
-																			className='product-page-button mt-3 w-100'
-																			type='submit'
-																			variant='primary'
-																		>
-																			投稿
-																		</Button>
-																	</Form>
-																</>
-															)}
-														</div>
-													</Col>
-												</Row>
-											</div>
+											<CustomerReview
+												product={product}
+												match={match}
+												userInfo={userInfo}
+											/>
 										)}
 										<div className='product-page-option-wrap'>
 											<Col
 												md={3}
+												sm={2}
 												className='product-page-price'
 											>
 												¥{product.price}
@@ -320,6 +136,7 @@ const ProductScreen = ({ match, history }) => {
 															'center',
 													}}
 													md={4}
+													sm={5}
 												>
 													{/* <Form.Control
 														className='form-select form-control-select'
@@ -401,7 +218,7 @@ const ProductScreen = ({ match, history }) => {
 													</div>
 												</Col>
 											)}
-											<Col md={5}>
+											<Col md={5} sm={6}>
 												<Button
 													onClick={addToCartHandler}
 													className='product-page-button'
