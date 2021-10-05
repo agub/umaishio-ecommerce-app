15f1537572ff4b0b3356dd98cb212051de9c91f5
agub <shintrfc@gmail.com>
2021-10-05 16:57:03 +0900

    contact form css add & landingpage comp order change

--- frontend/src/screens/ContactScreen.js
+++ frontend/src/screens/ContactScreen.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import FormContainer from '../components/FormContainer'
+import ModalContainer from '../components/ModalContainer'
 import { Button, Form } from 'react-bootstrap'
 import axios from 'axios'
 import Message from '../components/Message'
@@ -44,8 +45,8 @@ const ContactScreen = () => {
 
 	return (
 		<>
-			<FormContainer>
-				<h1>お問い合わせ</h1>
+			<ModalContainer>
+				<h1 className='text-center'>お問い合わせ</h1>
 				<Form onSubmit={submitHandler}>
 					{responseMsg && (
 						<Message>お問い合わせを受付ました。</Message>
@@ -54,56 +55,61 @@ const ContactScreen = () => {
 					<Form.Group controlId='postalCode' className='mt-2'>
 						<Form.Label>
 							<div className='shipping-form-lable__g'>
-								Email{' '}
-								<span className='shipping-form-icon__g'>
-									必須
-								</span>
+								メールアドレス{' '}
+								<span className='form-asterisk__g'>*</span>
 							</div>
 						</Form.Label>
-						<Form.Control
-							type='email'
-							value={email}
-							required
-							onChange={(e) => setEmail(e.target.value)}
-						></Form.Control>
+						<div className='form-container-pw-icon__g'>
+							<Form.Control
+								type='email'
+								value={email}
+								required
+								placeholder='メールアドレス'
+								onChange={(e) => setEmail(e.target.value)}
+							></Form.Control>
+						</div>
 					</Form.Group>
 					<Form.Group controlId='postalCode' className='mt-2'>
 						<Form.Label>
 							<div className='shipping-form-lable__g'>
-								氏名{' '}
-								<span className='shipping-form-icon__g'>
-									必須
-								</span>
+								氏名 <span className='form-asterisk__g'>*</span>
 							</div>
 						</Form.Label>
-						<Form.Control
-							type='string'
-							value={fullName}
-							required
-							onChange={(e) => setFullName(e.target.value)}
-						></Form.Control>
+						<div className='form-container-pw-icon__g'>
+							<Form.Control
+								type='string'
+								value={fullName}
+								required
+								placeholder='氏名'
+								onChange={(e) => setFullName(e.target.value)}
+							></Form.Control>
+						</div>
 					</Form.Group>
 					<Form.Group controlId='postalCode' className='mt-2'>
 						<Form.Label>
 							<div className='shipping-form-lable__g'>
-								内容{' '}
-								<span className='shipping-form-icon__g'>
-									必須
-								</span>
+								内容 <span className='form-asterisk__g'>*</span>
 							</div>
 						</Form.Label>
-						<Form.Control
-							type='text'
-							as='textarea'
-							value={content}
-							style={{ height: 200 }}
-							required
-							onChange={(e) => setContent(e.target.value)}
-						></Form.Control>
+						<div className='form-container-pw-icon__g'>
+							<Form.Control
+								type='text'
+								placeholder='内容'
+								as='textarea'
+								value={content}
+								style={{ height: 200 }}
+								required
+								onChange={(e) => setContent(e.target.value)}
+							></Form.Control>
+						</div>
 					</Form.Group>
-					<Button type='submit'>送信</Button>
+
+					<Button type='submit' className='next-gradient-btn__g mt-4'>
+						送信
+						<i className='fas fa-chevron-right fa-position-right'></i>
+					</Button>
 				</Form>
-			</FormContainer>
+			</ModalContainer>
 		</>
 	)
 }
