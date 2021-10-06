723da28aba5dd30f974f17bcd3762059dd1279ae
agub <shintrfc@gmail.com>
2021-10-06 20:38:11 +0900

    Rules add & ScrollToTop add

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
 import Aos from 'aos'
 import 'aos/dist/aos.css'
+import ScrollToTop from './hooks/ScrollToTop'
 
 import Header from './components/Header'
 import Footer from './components/Footer'
@@ -32,6 +33,7 @@ import LandingScreen from './screens/LandingScreen'
 import { Elements } from '@stripe/react-stripe-js'
 import { loadStripe } from '@stripe/stripe-js'
 import ResetPasswordScreen from './screens/ResetPasswordScreen'
+import RulesScreen from './screens/RulesScreen'
 
 function App() {
 	// const PUBLIC_KEY =
@@ -47,73 +49,87 @@ function App() {
 
 	return (
 		<Router>
-			<Elements stripe={stripePromise}>
-				<Header />
-				<main className='py-3 main-bgc'>
-					<Container>
-						<Route path='/' component={LandingScreen} exact />
-						<span style={{ color: 'red' }}>
-							＊現在このサイトはメンテナンス中です。お買い求めの際はお問い合わせフォームからお問い合わせください。
-						</span>
-						<Route path='/order/:id' component={OrderScreen} />
-						<Route path='/shipping' component={ShippingScreen} />
-						<Route
-							path='/guestshipping'
-							component={GuestShippingScreen}
-						/>
-						<Route
-							path='/forgot'
-							component={ForgotPasswordScreen}
-						/>
-						<Route
-							path='/password-reset/:id'
-							component={ResetPasswordScreen}
-						/>
-						<Route
-							path='/checkemail'
-							component={CheckEmailScreen}
-						/>
-						<Route
-							path='/placeorder'
-							component={PlaceOrderScreen}
-						/>
-						<Route path='/login' component={LoginScreen} />
-						<Route path='/register' component={RegisterScreen} />
-						<Route path='/profile' component={ProfileScreen} />
-						<Route path='/contact' component={ContactScreen} />
-						<Route path='/product/:id' component={ProductScreen} />
-						<Route path='/cart/:id?' component={CartScreen} />
-						<Route
-							path='/admin/userlist'
-							component={UserListScreen}
-						/>
-						<Route
-							path='/admin/user/:id/edit'
-							component={UserEditScreen}
-						/>
-						<Route
-							path='/admin/productlist'
-							component={ProductListScreen}
-							exact
-						/>
-						<Route
-							path='/admin/product/:id/edit'
-							component={ProductEditScreen}
-						/>
-						<Route
-							path='/admin/orderlist'
-							component={OrderListScreen}
-						/>
-						<Route
-							path='/verify/:id/:token'
-							component={VerifyUserScreen}
-						/>
-						<Route path='/shop' component={HomeScreen} />
-					</Container>
-				</main>
+			<ScrollToTop>
+				<Elements stripe={stripePromise}>
+					<Header />
+					<main className='py-3 main-bgc'>
+						<Container>
+							<Route path='/' component={LandingScreen} exact />
+							<span style={{ color: 'red' }}>
+								＊現在このサイトはメンテナンス中です。お買い求めの際はお問い合わせフォームからお問い合わせください。
+							</span>
+							<Route path='/order/:id' component={OrderScreen} />
+							<Route
+								path='/shipping'
+								component={ShippingScreen}
+							/>
+							<Route
+								path='/guestshipping'
+								component={GuestShippingScreen}
+							/>
+							<Route
+								path='/forgot'
+								component={ForgotPasswordScreen}
+							/>
+							<Route
+								path='/password-reset/:id'
+								component={ResetPasswordScreen}
+							/>
+							<Route
+								path='/checkemail'
+								component={CheckEmailScreen}
+							/>
+							<Route
+								path='/placeorder'
+								component={PlaceOrderScreen}
+							/>
+							<Route path='/login' component={LoginScreen} />
+							<Route
+								path='/register'
+								component={RegisterScreen}
+							/>
+							<Route path='/profile' component={ProfileScreen} />
+							<Route path='/contact' component={ContactScreen} />
+							<Route
+								path='/product/:id'
+								component={ProductScreen}
+							/>
+							<Route path='/cart/:id?' component={CartScreen} />
+							<Route
+								path='/admin/userlist'
+								component={UserListScreen}
+							/>
+							<Route
+								path='/admin/user/:id/edit'
+								component={UserEditScreen}
+							/>
+							<Route
+								path='/admin/productlist'
+								component={ProductListScreen}
+								exact
+							/>
+							<Route
+								path='/admin/product/:id/edit'
+								component={ProductEditScreen}
+							/>
+							<Route
+								path='/admin/orderlist'
+								component={OrderListScreen}
+							/>
+							<Route
+								path='/verify/:id/:token'
+								component={VerifyUserScreen}
+							/>
 
-				<Footer />
-			</Elements>
+							<Route path='/rules' component={RulesScreen} />
+
+							<Route path='/shop' component={HomeScreen} />
+						</Container>
+					</main>
+
+					<Footer />
+				</Elements>
+			</ScrollToTop>
 		</Router>
 	)
 }
--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import { Container, Row, Col, Image } from 'react-bootstrap'
 import logo from '../data/images/logo.png'
 
@@ -6,19 +7,41 @@ const Footer = () => {
 	return (
 		<footer style={{ backgroundColor: '#1a1a1a' }}>
 			<Container>
-				<div style={{ display: 'flex', justifyContent: 'center' }}>
+				<div
+					style={{
+						display: 'flex',
+						justifyContent: 'center',
+					}}
+				>
 					<Image
 						src={logo}
 						alt='logo'
 						style={{ width: '200px', margin: '20px' }}
 					/>
 				</div>
-				<Row className='text-center py-3'>
-					<Col>info@umaishio.com</Col>
-				</Row>
 
-				<Row className='text-center py-3'>
-					<Col>Copyright &copy; 旨い塩</Col>
+				<div
+					style={{
+						display: 'flex',
+						justifyContent: 'center',
+					}}
+				>
+					<Link to='/rules'>
+						<p
+							// className='text-center'
+							style={{ color: 'white', fontSize: 10 }}
+						>
+							<br />
+							特定商取引法に基づく表記はこちらから
+							<br />
+						</p>
+					</Link>
+				</div>
+				<Row
+					style={{ color: 'white', fontSize: 10 }}
+					className='text-center pb-3'
+				>
+					<Col> Copyright &copy; 旨い塩</Col>
 				</Row>
 			</Container>
 		</footer>
